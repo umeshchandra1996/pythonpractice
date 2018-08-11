23f58b2ec9724ea5b47b7c85c70fcc2f2a19a982
umeshchandra1996 <37735156+umeshchandra1996@users.noreply.github.com>
2018-08-11 23:17:28 +0530

    Update bottle_conting.py

--- bottle_conting.py
+++ bottle_conting.py
@@ -1,13 +1,13 @@
-
-for qaunt in range(50,0,-1):
-    if qaunt>1:
-        print(qaunt,"Bottel of beer on the wall"  ,qaunt,"Bottel of beer")
-        if qaunt>2:
-            suffix=str(qaunt) + "bottel of beer the wall"
+for quant in range(50,0,-1):
+    if quant>1:
+        print(quant,"Bottel of beer on the wall"  ,quant,"Bottel of beer")
+        
+        if quant>2:
+            suffix=str(quant) + "bottel of beer the wall"
         else:
             suffix="1 bottel of on the wall"
             
-    elif qaunt == 1:
+    elif quant == 1:
         print("1 bottel of beer on the wall, 1 bottel of beer")
         suffix="No more beer on the wall"
 
