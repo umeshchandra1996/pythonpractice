cbb678ac0e19876fe21ae413eefaf3ee6a9ae55e
umeshchandra1996 <37735156+umeshchandra1996@users.noreply.github.com>
2018-08-12 00:19:35 +0530

    Update grade_marks_of_student.py

--- grade_marks_of_student.py
+++ grade_marks_of_student.py
@@ -18,3 +18,47 @@ elif(ave>=40):
 	print("GRADE : E")
 else:
 	print("FAILED")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+studentMarks=[]
+noOfSub = int(input("Enter the no of subject\n"))
+noOfSubi =noOfSub
+while noOfSub>0:
+    marks=int(input("Enter the marks\n"))
+    studentMarks.append(marks)
+    noOfSub=noOfSub-1
+    
+sum=0
+for i in range (0,noOfSubi):
+    sum+=studentMarks[i]
+    
+ave=sum/noOfSubi
+print(str(ave)+"\n")
+if(ave>=90):
+	print("GRADE : O")
+elif(ave>=80):
+	print("GRADE : A")
+elif(ave>=70):
+	print("GRADE : B")
+elif(ave>=60):
+	print("GRADE : C")
+elif(ave>=50):
+	print("GRADE : D")
+elif(ave>=40):
+	print("GRADE : E")
+else:
+	print("FAILED")
+
+
+
